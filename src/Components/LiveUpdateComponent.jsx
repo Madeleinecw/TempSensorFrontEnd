2bff0f815ee7f45e1d90ead8846aa547c0365f3f
Madeleine Wood <madeleinecwood@gmail.com>
2021-04-12 10:34:42 +0100

    fix: time format for live update

--- src/Components/LiveUpdateComponent.jsx
+++ src/Components/LiveUpdateComponent.jsx
@@ -14,16 +14,18 @@ const LiveUpdatesComponent = () => {
         let unformatedStart = new Date(unformatedEnd);
 
         unformatedStart.setDate(unformatedStart.getDate() - 1)
+        unformatedStart.setSeconds(0,0);
+        unformatedEnd.setSeconds(0,0);
 
-        let start = new Date(unformatedStart).toISOString();
-        let end = new Date(unformatedEnd).toISOString();
+        let start = new Date(unformatedStart).toISOString().replace(/:00.000Z/, "");
+        let end = new Date(unformatedEnd).toISOString().replace(/:00.000Z/, "");
 
         fetch(`http://192.168.1.237:5000/getgraph/${start}/${end}`)
                 .then(response => response.json())
                 .then(data => setFormattedData(formatData(data)));
 
         MyResponsiveLine(formattedData)
-    })
+    },[])
  
     return (
         <div className="liveGraph-container">
